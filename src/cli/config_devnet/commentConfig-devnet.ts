f84bb23a287502839d4ba13140f3d9e3e43c43df
SolCharms <ckioulos@uottawa.ca>
2023-04-24 00:19:02 -0400

    clean up after reorg

--- src/cli/config_devnet/commentConfig-devnet.ts
+++ src/cli/config_devnet/commentConfig-devnet.ts
@@ -6,16 +6,26 @@ type CommentConfig = {
     content: string,
 }
 
+// // User 1
+// export const commentConfig: CommentConfig =
+//     {
+//         commentedOn:  new PublicKey("7JUWv7LtxSWWCZPaNUjQvxn9tS69iXcV9B66CaYWBVvd"), // must be question or answer account key
+//         forum: new PublicKey("J462agvz9tZyNFYgBgPwbnXDvf1qhGpwGrmEYowQrky3"),
+//         content: "Please remove the excess tags which do not apply.",
+//     }
 
+// // User 4
+// export const commentConfig: CommentConfig =
+//     {
+//         commentedOn:  new PublicKey("7JUWv7LtxSWWCZPaNUjQvxn9tS69iXcV9B66CaYWBVvd"), // must be question or answer account key
+//         forum: new PublicKey("J462agvz9tZyNFYgBgPwbnXDvf1qhGpwGrmEYowQrky3"),
+//         content: "Hey, leave my engagement farming alone! I'm just here trying to get some alpha.",
+//     }
+
+// User 2
 export const commentConfig: CommentConfig =
     {
-        commentedOn:  new PublicKey("FwvVcNLHdw4fo1B6jhEGpJoa8jXD493jNWmGnWy31Cmf"),
-        forum: new PublicKey("FwvVcNLHdw4fo1B6jhEGpJoa8jXD493jNWmGnWy31Cmf"),
-        content: "This is a sample answer to a question.",
+        commentedOn:  new PublicKey("DZGzyKtKjSnR6Q4A29n1nc7RsLKsCqGm7GsKb4pY4fRd"), // must be question or answer account key
+        forum: new PublicKey("J462agvz9tZyNFYgBgPwbnXDvf1qhGpwGrmEYowQrky3"),
+        content: "LOL! DeGods are long gone dude.",
     }
-
-
-export const additionalCommentContent: string[] =
-    [
-        "This is additional content to the comment."
-    ]
--- src/cli/config_devnet/networkConfig-devnet.ts
+++ src/cli/config_devnet/networkConfig-devnet.ts
@@ -3,16 +3,51 @@ type NetworkConfig = {
     signerKeypair: string
 }
 
-// Forum Manager Account
-export const networkConfig: NetworkConfig =
-    {
-        clusterApiUrl: "https://api.devnet.solana.com",
-        signerKeypair: "/home/SolCharms/.config/solana/devnet-forum/forum_manager.json"
-    }
-
-// // User 1 Account
+// // Forum Manager Account
 // export const networkConfig: NetworkConfig =
 //     {
 //         clusterApiUrl: "https://api.devnet.solana.com",
-//         signerKeypair: "/home/SolCharms/.config/solana/devnet-forum/user_1.json"
+//         signerKeypair: "/home/charalambos/.config/solana/devnet-forum/forum_manager.json"
+//     }
+
+// User 1 Account
+export const networkConfig: NetworkConfig =
+    {
+        clusterApiUrl: "https://api.devnet.solana.com",
+        signerKeypair: "/home/charalambos/.config/solana/devnet-forum/user_1.json"
+    }
+
+// // User 2 Account
+// export const networkConfig: NetworkConfig =
+//     {
+//         clusterApiUrl: "https://api.devnet.solana.com",
+//         signerKeypair: "/home/charalambos/.config/solana/devnet-forum/user_2.json"
+//     }
+
+// // User 3 Account
+// export const networkConfig: NetworkConfig =
+//     {
+//         clusterApiUrl: "https://api.devnet.solana.com",
+//         signerKeypair: "/home/charalambos/.config/solana/devnet-forum/user_3.json"
+//     }
+
+// // User 4 Account
+// export const networkConfig: NetworkConfig =
+//     {
+//         clusterApiUrl: "https://api.devnet.solana.com",
+//         signerKeypair: "/home/charalambos/.config/solana/devnet-forum/user_4.json"
+//     }
+
+// // User 5 Account
+// export const networkConfig: NetworkConfig =
+//     {
+//         clusterApiUrl: "https://api.devnet.solana.com",
+//         signerKeypair: "/home/charalambos/.config/solana/devnet-forum/user_5.json"
+//     }
+
+// // Deploy Key Account
+// export const networkConfig: NetworkConfig =
+//     {
+//         clusterApiUrl: "https://api.devnet.solana.com",
+//         signerKeypair: "/home/charalambos/.config/solana/devnet-test1.json"
 //     }
